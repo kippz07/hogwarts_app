60e90391e8bb1030e89ebff4190e2cacd0aabf3a
Jenny Johnson <kippz07@users.noreply.github.com>
2017-07-20 19:52:58 +0100

    applied last change to all houses

--- app/controllers/slytherin_controller.rb
+++ app/controllers/slytherin_controller.rb
@@ -2,12 +2,14 @@ class SlytherinController < ApplicationController
   before_action :authenticate_user!
   def index
     @users = users
+    @curr_user = current_user
     spells = Spell.all
     @indi_spells = GreatHall.get_spells(@users, spells)
   end
 
   def show
     id = params[:id].to_i
+    @curr_user = current_user
     @user = users[id - 1]
     spells = Spell.all
     @indi_spells = GreatHall.get_indi_spells(@user, spells)
